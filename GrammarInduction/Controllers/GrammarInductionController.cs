d287145b048edfb2bdbf180109c15ac1bdadd22d
Joseph Potashnik <joseph.potashnik@gmail.com>
2019-01-04 16:30:10 +0000

    Add project files.

---
+++ GrammarInduction/Controllers/GrammarInductionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrammarInduction.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SyntaxInduction")]
+    public class GrammarInductionController : Controller
+    {
+        public void Learn(string fileName)
+        {
+
+        }
+    }
+}
